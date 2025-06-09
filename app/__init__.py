5cb3688fd06a3f711603e6595348740b478c0d1a
Amit Rangra <amitrangra@microsoft.com>
2025-06-09 20:50:21 +0100

    Fix: Update SQLite database path to use writable directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 db = SQLAlchemy()
 
@@ -8,7 +9,8 @@ def create_app(config=None):
     app = Flask(__name__)
     
     # Default configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+    db_path = os.path.join(os.environ.get('HOME', '/home/site/wwwroot'), 'todos.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Override with custom config if provided
